c9b0a5380f0fa269755266d082bf591848d6f4e8
sslokk <slok123low@gmail.com>
2021-06-29 22:18:28 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,94 @@
+var apiKey = "534e73de";
+
+var button = document.getElementById("searchButton");
+var movieTitle = document.getElementById("searchText").value;
+var list = document.getElementById("list");
+var numClicked = 0;
+var searchOptionsTxt;
+var pageNumber = 1;
+var prevButton = document.createElement("button");
+var nextButton = document.createElement("button");
+var numOfResults;
+var numOfResultsParagraph = document.createElement("p");
+var h1Tag = document.createElement("h1");
+
+function showResults(data) {
+    var orderedList = document.getElementById("list");
+    let toMovieDetails;
+    var temp;
+    
+    for (let item in data.Search) {
+        toMovieDetails = document.createElement("a");
+        toMovieDetails.href = "movieDetails.html";
+        var listElement = document.createElement("li");
+        listElement.appendChild(toMovieDetails);
+        toMovieDetails.innerHTML = data.Search[item].Title;
+        orderedList.appendChild(listElement);
+        toMovieDetails.onclick = function() {
+            sessionStorage.setItem("movie", data.Search[item].Title);
+        }
+    }
+    
+    numOfResults = Math.ceil(data.totalResults/10);
+    h1Tag.id = "h1Tag";
+    h1Tag.innerHTML = "Results";
+    addNavigationButtons();
+
+    numOfResultsParagraph.id = "numOfResultsParagraph";
+    list.insertAdjacentElement("afterend", numOfResultsParagraph);
+
+    temp = document.getElementById("searchButton");
+    temp.insertAdjacentElement("afterend", h1Tag);
+    
+    numOfResultsParagraph.innerHTML = `Page ${pageNumber} of ${numOfResults} (${data.totalResults} results total)`;
+
+
+}
+
+
+button.onclick = function () {
+    h1Tag.innerHTML = ``;
+    numOfResultsParagraph.innerHTML = ``;
+    list.innerHTML = ``;
+    search(searchText.value);
+}
+
+function search(movieTitle) {
+    searchOptionsTxt = document.getElementById("searchOptionsTxt").value;
+    var encodedTitle = encodeURI(movieTitle);
+
+    var apiURL = `http://www.omdbapi.com/` + `?s=${encodedTitle}` + `&type=movie`+ `&y=${searchOptionsTxt}`+ `&page=${pageNumber}` + `&apikey=${apiKey}`;
+
+    fetch(apiURL)
+        .then(response => response.json())
+        .then(data => showResults(data));
+
+}
+
+nextButton.onclick = function() {
+    if(pageNumber != numOfResults) {
+        pageNumber++;
+        list.innerHTML = ``;
+        numOfResultsParagraph.innerHTML = ``;
+        search(searchText.value);
+    }
+}
+
+prevButton.onclick = function() {
+    if(pageNumber != 1) {
+        pageNumber--;
+        list.innerHTML = ``;
+        numOfResultsParagraph.innerHTML = ``;
+        search(searchText.value);
+    }
+}
+
+function addNavigationButtons() {
+
+    prevButton.innerHTML = "Prev";
+    nextButton.innerHTML = "Next";
+
+    list.insertAdjacentElement("afterend", nextButton);
+    list.insertAdjacentElement("afterend", prevButton);
+
+}
---
+++ movie.js
@@ -0,0 +1,38 @@
+let storedMovieTitle = sessionStorage.getItem("movie");
+var apiKey = "534e73de";
+var generalInfo = document.getElementById("generalInfo");
+var rating = document.getElementById("rating");
+var director = document.getElementById("director");
+var plotSummary = document.getElementById("plotSummary");
+var moviePoster = document.getElementById("moviePoster");
+
+window.onload = function() {
+    search();
+}
+
+
+async function search() {
+    var encodedTitle = encodeURI(storedMovieTitle);
+
+    var apiURL = `http://www.omdbapi.com/` + `?t=${encodedTitle}` + `&type=movie`+ `&plot=full` + `&apikey=${apiKey}`;
+    console.log(apiURL);
+        
+    var movieDetails = await fetch(apiURL)
+        .then(response => response.json())
+
+    console.log(movieDetails);
+
+    var movieTitle = document.getElementById("movieTitle");
+    movieTitle.innerHTML = storedMovieTitle;
+    document.title = storedMovieTitle;
+
+    generalInfo.innerHTML = movieDetails.Released + " | " + movieDetails.Runtime + " | " + movieDetails.Genre;
+    rating.innerHTML = "IMDb Rating: " + movieDetails.imdbRating + "/10";
+    director.innerHTML = "Directed by " + movieDetails.Director;
+
+    moviePoster.src = movieDetails.Poster;
+    moviePoster.alt = "Movie Poster";
+
+    plotSummary.innerHTML = movieDetails.Plot;
+}
+
