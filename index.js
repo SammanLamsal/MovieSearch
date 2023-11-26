31fb7c88980162f32660883d9734d4c99dcf859e
Samman Lamsal <lamsal.samman@gmail.com>
2023-11-25 22:51:58 -0500

    Allowed enter key press for results to be shown

--- index.js
+++ index.js
@@ -13,85 +13,87 @@ var numOfResultsParagraph = document.createElement("p");
 var h1Tag = document.createElement("h1");
 
 function showResults(data) {
-    var orderedList = document.getElementById("list");
-    let toMovieDetails;
-    var temp;
+  var orderedList = document.getElementById("list");
+  let toMovieDetails;
+  var temp;
 
-    if(data.Response == "False") {
-        numOfResultsParagraph.innerHTML = "Movie not found!";
+  if (data.Response == "False") {
+    numOfResultsParagraph.innerHTML = "Movie not found!";
+  } else {
+    for (let item in data.Search) {
+      toMovieDetails = document.createElement("a");
+      toMovieDetails.href = "movieDetails.html";
+      var listElement = document.createElement("li");
+      listElement.appendChild(toMovieDetails);
+      toMovieDetails.innerHTML = data.Search[item].Title;
+      orderedList.appendChild(listElement);
+      toMovieDetails.onclick = function () {
+        sessionStorage.setItem("movie", data.Search[item].Title);
+      };
     }
-    else{
-        for (let item in data.Search) {
-            toMovieDetails = document.createElement("a");
-            toMovieDetails.href = "movieDetails.html";
-            var listElement = document.createElement("li");
-            listElement.appendChild(toMovieDetails);
-            toMovieDetails.innerHTML = data.Search[item].Title;
-            orderedList.appendChild(listElement);
-            toMovieDetails.onclick = function() {
-                sessionStorage.setItem("movie", data.Search[item].Title);
-            }
-        }
-        numOfResults = Math.ceil(data.totalResults/10);
-        numOfResultsParagraph.innerHTML = `Page ${pageNumber} of ${numOfResults} (${data.totalResults} results total)`;
-    }
-        h1Tag.id = "h1Tag";
-        h1Tag.innerHTML = "Results";
-        addNavigationButtons();
+    numOfResults = Math.ceil(data.totalResults / 10);
+    numOfResultsParagraph.innerHTML = `Page ${pageNumber} of ${numOfResults} (${data.totalResults} results total)`;
+  }
+  h1Tag.id = "h1Tag";
+  h1Tag.innerHTML = "Results";
+  addNavigationButtons();
 
-        numOfResultsParagraph.id = "numOfResultsParagraph";
-        list.insertAdjacentElement("afterend", numOfResultsParagraph);
+  numOfResultsParagraph.id = "numOfResultsParagraph";
+  list.insertAdjacentElement("afterend", numOfResultsParagraph);
 
-        temp = document.getElementById("searchButton");
-        temp.insertAdjacentElement("afterend", h1Tag);
-
+  temp = document.getElementById("searchButton");
+  temp.insertAdjacentElement("afterend", h1Tag);
 }
 
-
-button.onclick = function () {
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
     h1Tag.innerHTML = ``;
     numOfResultsParagraph.innerHTML = ``;
     list.innerHTML = ``;
     search(searchText.value);
-}
+  }
+});
+
+button.onclick = function () {
+  h1Tag.innerHTML = ``;
+  numOfResultsParagraph.innerHTML = ``;
+  list.innerHTML = ``;
+  search(searchText.value);
+};
 
 function search(movieTitle) {
-    searchOptionsTxt = document.getElementById("searchOptionsTxt").value;
-    console.log(searchOptionsTxt)
-    var encodedTitle = encodeURI(movieTitle);
+  searchOptionsTxt = document.getElementById("searchOptionsTxt").value;
+  var encodedTitle = encodeURI(movieTitle);
 
-    var apiURL = `https://www.omdbapi.com/` + `?s=${encodedTitle}` + `&type=movie`+ `&y=${searchOptionsTxt}`+ `&page=${pageNumber}` + `&apikey=${apiKey}`;
+  var apiURL = `https://www.omdbapi.com/` + `?s=${encodedTitle}` + `&type=movie` + `&y=${searchOptionsTxt}` + `&page=${pageNumber}` + `&apikey=${apiKey}`;
 
-    fetch(apiURL)
-        .then(response => response.json())
-        .then(data => showResults(data));
-
+  fetch(apiURL)
+    .then((response) => response.json())
+    .then((data) => showResults(data));
 }
 
-nextButton.onclick = function() {
-    if(pageNumber != numOfResults) {
-        pageNumber++;
-        list.innerHTML = ``;
-        numOfResultsParagraph.innerHTML = ``;
-        search(searchText.value);
-    }
-}
+nextButton.onclick = function () {
+  if (pageNumber != numOfResults) {
+    pageNumber++;
+    list.innerHTML = ``;
+    numOfResultsParagraph.innerHTML = ``;
+    search(searchText.value);
+  }
+};
 
-prevButton.onclick = function() {
-    if(pageNumber != 1) {
-        pageNumber--;
-        list.innerHTML = ``;
-        numOfResultsParagraph.innerHTML = ``;
-        search(searchText.value);
-    }
-}
+prevButton.onclick = function () {
+  if (pageNumber != 1) {
+    pageNumber--;
+    list.innerHTML = ``;
+    numOfResultsParagraph.innerHTML = ``;
+    search(searchText.value);
+  }
+};
 
 function addNavigationButtons() {
+  prevButton.innerHTML = "Prev";
+  nextButton.innerHTML = "Next";
 
-    prevButton.innerHTML = "Prev";
-    nextButton.innerHTML = "Next";
-
-    list.insertAdjacentElement("afterend", nextButton);
-    list.insertAdjacentElement("afterend", prevButton);
-
+  list.insertAdjacentElement("afterend", nextButton);
+  list.insertAdjacentElement("afterend", prevButton);
 }
